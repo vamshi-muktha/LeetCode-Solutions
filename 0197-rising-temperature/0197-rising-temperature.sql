9432622ae07eb1ea4e227fe55b3c7b3d8f66bdb9
vamshi-muktha <141153452+vamshi-muktha@users.noreply.github.com>
2026-02-09 15:55:33 +0530

    Time: 508 ms (25.59%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,2 +1,2 @@
-select a.id from weather a join weather b on DATEDIFF(a.recordDate, b.recordDate) = 1
+select a.id from weather a cross join weather b on DATEDIFF(a.recordDate, b.recordDate) = 1
 and b.temperature < a.temperature
